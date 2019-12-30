290fea19fe7d35b5d2e2cbece82eed1cd6f5517f
Remi Dormoy <rdormoy@octo.com>
2019-12-30 15:54:27 +0100

    added pager animation

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 const double pi = 3.1415926535897932;
 
@@ -23,32 +24,24 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   Animation<double> _scale;
+  Animation<double> _scalePageView;
   Animation<double> _angle;
   AnimationController _controller;
   AnimationController _controller2;
   AnimationController _controller3;
   AnimationController _controller4;
+  AnimationController _controller5;
   AnimationController _currentController;
+  bool _pagerVisible = false;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      duration: new Duration(milliseconds: 1300),
-      vsync: this,
-    );
-    _controller2 = AnimationController(
-      duration: new Duration(milliseconds: 300),
-      vsync: this,
-    );
-    _controller3 = AnimationController(
-      duration: new Duration(milliseconds: 300),
-      vsync: this,
-    );
-    _controller4 = AnimationController(
-      duration: new Duration(milliseconds: 300),
-      vsync: this,
-    );
+    initControllers();
+    playPhase1();
+  }
+
+  void playPhase1() {
     _currentController = _controller;
     _scale = Tween<double>(
       begin: 1.00,
@@ -83,20 +76,96 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     _controller.forward();
   }
 
+  void initControllers() {
+    _controller = AnimationController(
+      duration: new Duration(milliseconds: 1300),
+      vsync: this,
+    );
+    _controller2 = AnimationController(
+      duration: new Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _controller3 = AnimationController(
+      duration: new Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _controller4 = AnimationController(
+      duration: new Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _controller5 = AnimationController(
+      duration: new Duration(milliseconds: 500),
+      vsync: this,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    var body;
+    if (_pagerVisible) {
+      //print('le scale est à : ' + _scalePageView.value.toString());
+      body = AnimatedBuilder(
+        builder: _buildPagerAnimation,
+        animation: _controller5,
+      );
+    } else {
+      body = Center(
           child: Column(
+            children: <Widget>[
+              AnimatedBuilder(
+                  animation: _currentController, builder: _buildAnimation),
+              AnimatedBuilder(
+                animation: _currentController,
+                builder: _buildAnimationImage,
+              ),
+            ],
+          ));
+    }
+    return Scaffold(
+      body: body,
+    );
+  }
+
+  Widget _buildPagerAnimation(BuildContext context, Widget child) {
+    return Transform.scale(
+      scale: _scalePageView.value,
+      child: PageView(
         children: <Widget>[
-          AnimatedBuilder(
-              animation: _currentController, builder: _buildAnimation),
-          AnimatedBuilder(
-            animation: _currentController,
-            builder: _buildAnimationImage,
+          Column(
+            children: <Widget>[
+              Expanded(
+                child: Image(image: AssetImage('assets/ic_loupe.png')),
+              ),
+              Text('Découvrez'),
+              Text('Découvrez nos dix tendances de conception mobile en 2020, présentées  à travers un parcours bancaire.')
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Expanded(
+                child: SvgPicture.asset(
+                    'notification.svg',
+                    semanticsLabel: 'Expérimentez'
+                ),
+              ),
+              Text('Expérimentez'),
+              Text('Promenez vous sur l’application et cliquez sur les pastilles pour en apprendre plus')
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Expanded(
+                child: SvgPicture.asset(
+                    'diamond.svg',
+                    semanticsLabel: 'Lancez vous !'
+                ),
+              ),
+              Text('Lancez vous !'),
+              Text('Découvrez nos dix tendances de conception mobile en 2020, présentées  à travers un parcours bancaire.')
+            ],
           ),
         ],
-      )),
+      ),
     );
   }
 
@@ -186,7 +255,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       if (status == AnimationStatus.completed) {
         _playPhase4();
       }
-    });;
+    });
     _angle = Tween<double>(
       begin: 0,
       end: -pi / 6.0,
@@ -232,10 +301,34 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           curve: Curves.easeIn,
         ),
       ),
-    );
+    )..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _playPhase5();
+      }
+    });
     _controller4.forward();
     setState(() {
       _currentController = _controller4;
+    });
+  }
+
+  void _playPhase5() {
+    _scalePageView = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller5,
+        curve: Interval(
+          0.0,
+          1.0,
+          curve: Curves.easeIn,
+        ),
+      ),
+    );
+    _controller5.forward();
+    setState(() {
+      _pagerVisible = true;
     });
   }
 }
