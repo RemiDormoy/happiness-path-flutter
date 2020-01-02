723c0f83aa537fb26185d7aa26b482f1c26350ee
Remi Dormoy <rdormoy@octo.com>
2020-01-02 10:55:53 +0100

    added rest of stuff in operation screen. Just missing bottom sheet

--- lib/OperationList.dart
+++ lib/OperationList.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:happiness_path/colors.dart';
+import 'package:happiness_path/shortcutList.dart';
 
 import 'actionCell.dart';
 import 'endOfListCell.dart';
 import 'operationCell.dart';
 
 class OperationList extends StatelessWidget {
-
   ScrollController _controller;
 
   OperationList(ScrollController controller) {
@@ -14,12 +15,78 @@ class OperationList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var titleStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 16);
     return ListView(
       controller: _controller,
       children: <Widget>[
         Container(
-          height: 400,
+          height: 375,
           child: Text(''),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Raccourcis',
+            style: titleStyle,
+          ),
+        ),
+        ShortcutList(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Historique',
+            style: titleStyle,
+          ),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: double.infinity),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Flexible(
+                child: FlatButton(
+                  color: colorPrimaryDark,
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: Text('Tout'),
+                  ),
+                  onPressed: () => {},
+                ),
+                flex: 2,
+              ),
+              Flexible(
+                child: FlatButton(
+                  textColor: colorPrimaryDark,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: colorPrimaryDark),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: Text('Envoyé'),
+                  ),
+                  onPressed: () => {},
+                ),
+                flex: 2,
+              ),
+              Flexible(
+                child: FlatButton(
+                  textColor: colorPrimaryDark,
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: colorPrimaryDark),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: Text('Reçu'),
+                  ),
+                  onPressed: () => {},
+                ),
+                flex: 2,
+              ),
+            ],
+          ),
         ),
         OperationCell(200, 'Aniversaire Nicolas', '2019/09/08'),
         OperationCell(-40, 'Repas au Burger King', '2019/09/08'),
@@ -45,6 +112,4 @@ class OperationList extends StatelessWidget {
       ],
     );
   }
-
-
 }
---
+++ lib/shortcutList.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class ShortcutList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 141,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Shortcut('Transferts', alizouzBlue, Icons.compare_arrows),
+            Shortcut('Mes comptes', actionCardColor, Icons.lock),
+            Shortcut('Dashboards', actionCardColor, Icons.lock),
+            Shortcut('Mes cartes', actionCardColor, Icons.lock),
+          ],
+        ));
+  }
+}
+
+class Shortcut extends StatelessWidget {
+  String _label;
+  Color _color;
+  IconData _icon;
+
+  Shortcut(this._label, this._color, this._icon);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 125,
+        width: 140,
+        decoration: BoxDecoration(
+            color: _color,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 16.0, 0, 0),
+              child: IconTheme(
+                data: new IconThemeData(color: Colors.white),
+                child: Icon(_icon),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 0, 16, 16),
+                  child: ConstrainedBox(
+                    constraints:
+                        const BoxConstraints(minWidth: double.infinity),
+                    child: Text(_label,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white)),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
